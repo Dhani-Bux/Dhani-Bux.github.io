5e372b39527d639e56a8d53c09df083049dc6c2e
Ahmed Raza <52567156+ahmedraza55592@users.noreply.github.com>
2022-02-11 01:55:52 +0500

    My Portfolio
    My Portfolio

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slides').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slides-dots',
+        dotsClass:'dots',
+
+});
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+
+hamberger.addEventListener('click', function(){
+ mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+});
